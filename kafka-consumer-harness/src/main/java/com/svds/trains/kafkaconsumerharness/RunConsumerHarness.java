232db9f4a29ee1b2e89d208684cdecee3c045b81
Dmitriy Feferman <dmitriy@svds.com>
2016-06-13 10:08:24 -0500

    initial code, modified .gitignore

---
+++ kafka-consumer-harness/src/main/java/com/svds/trains/kafkaconsumerharness/RunConsumerHarness.java
@@ -0,0 +1,114 @@
+package com.svds.trains.kafkaconsumerharness;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
+import kafka.serializer.Decoder;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+
+public class RunConsumerHarness {
+
+    // private static final int WRITE_BATCH_SIZE = 1;
+
+    public static void main(String[] args) throws MalformedURLException,
+    IOException {
+
+        System.out.println(String.format(
+                " Running with props %s, topic %s,outputDir %s", args[0],
+                args[1], args[2]));
+
+        //TODO: figure out best size for buffer
+        final int WRITE_BATCH_SIZE = 1000;
+
+        Properties kafkaConsumerProps = new Properties();
+        kafkaConsumerProps.load(new java.net.URL(args[0]).openStream());
+
+        String topic = args[1];
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(
+                kafkaConsumerProps);
+
+        consumer.subscribe(Arrays.asList(topic));
+        TopicPartition partition0 = new TopicPartition(topic, 0);
+
+        System.out.println("starting with offset: " + consumer.committed(partition0));
+        Path outFile = Paths.get(args[2] + "/" + topic + ".out");
+
+        ArrayList<String> outputBuffer = new ArrayList<>();
+
+        try {
+            while (true) {                
+                ConsumerRecords<String, String> records = consumer.poll(1000);
+                if (records.count() > 0) {
+                    for (ConsumerRecord<String, String> record : records) {
+                        outputBuffer.add("Partition: " + record.partition() + ",Offset: " + record.offset() + ",Key:" + record.key() + ",Value:"+ record.value());
+                    }
+                    if (outputBuffer.size() >= WRITE_BATCH_SIZE) {
+                        writeBufferToFile(outFile, outputBuffer);
+                        consumer.commitSync();
+                        outputBuffer.clear();
+                    }
+                } else {
+                    if (outputBuffer.size() > 0) {
+                        System.out.println("clearing non-zero buffer");
+                        writeBufferToFile(outFile, outputBuffer);
+                        outputBuffer.clear();
+                    }
+                }
+            }
+        } finally {
+            if (outputBuffer.size() > 0) {
+                System.out.println("found non-zero buffer at exit, writing " + outputBuffer.size() + " records to file");
+                writeBufferToFile(outFile, outputBuffer);
+            }          
+            consumer.close();
+        }
+
+    }
+
+    public static Decoder<String> getStringDecoder() {
+        Decoder<String> decoder = new Decoder<String>() {
+            public String fromBytes(byte[] bytes) {
+                return bytes.toString();
+            }
+        };
+        return decoder;
+    }
+
+    private static void writeBufferToFile(Path outFile,
+            ArrayList<String> outputBuffer) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(outFile.toFile(), true));
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println("Writing buffer with length: " + outputBuffer.size()
+                + " to file: " + outFile.toString());
+
+        try {
+
+            for (String outString : outputBuffer) {
+                writer.append(outString + "\n");
+            }
+            writer.close();
+        } catch (IOException e1) {
+            System.err.println(e1.getMessage());
+        }
+
+        System.out.println("finished writing buffer");
+    }
+
+}
---
+++ kafka-producer-harness/src/main/java/com/svds/trains/kafkaproducerharness/ProduceTestItems.java
@@ -0,0 +1,86 @@
+package com.svds.trains.kafkaproducerharness;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
+
+//import uk.co.jemos.podam.api.PodamFactory;
+//import uk.co.jemos.podam.api.PodamFactoryImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Accepts Kafka cluster configurations, number of iterations. Sends items to
+ * the kafka cluster.
+ * 
+ *
+ */
+public class ProduceTestItems {
+
+    // public static PodamFactory podamFactory = new PodamFactoryImpl();
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws MalformedURLException,
+            IOException, InterruptedException, ExecutionException {
+
+        long start = System.currentTimeMillis();
+        Properties kafkaProducerProps = new Properties();
+
+        kafkaProducerProps.load(new java.net.URL(args[0]).openStream());
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(
+                kafkaProducerProps);
+
+        Map<MetricName, ? extends Metric> metrics = producer.metrics();
+
+        Integer iterations = Integer.parseInt(args[2]);
+
+        Integer i;
+
+        long prevMillis = start;
+
+        String topic = args[1];
+
+        int messagesSent = 0;
+        System.out.println("Running " + iterations + " iterations for topic: "
+                + topic);
+
+        for (i = 0; i < iterations; i++) {
+
+            producer.send(new ProducerRecord<String, String>(topic, i
+                    .toString(), "Message_" + i));
+            messagesSent++;
+
+            // Needed because otherwise we overwhelm the producer
+            Thread.sleep(1);
+            if (i % 10000 == 0) {
+                long currMillis = System.currentTimeMillis();
+                long setTime = currMillis - prevMillis;
+                System.out.println("sent " + messagesSent
+                        + " records so far in " + setTime);
+                prevMillis = currMillis;
+            }
+        }
+
+        producer.flush();
+
+        long end = System.currentTimeMillis();
+
+        long totalMillis = end - start;
+
+        System.out.println("Finished sending " + i + " records in: "
+                + totalMillis + " millis");
+
+    }
+
+}
